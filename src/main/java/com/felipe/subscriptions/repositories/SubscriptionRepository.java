fa873d79f7b310067247c8049722ad8dd64466e7
Felipe Velasque <fe.velasquee@gmail.com>
2025-09-02 20:45:56 -0300

    feat: add SubscriptionRepository

---
+++ src/main/java/com/felipe/subscriptions/repositories/SubscriptionRepository.java
@@ -0,0 +1,13 @@
+package com.felipe.subscriptions.repositories;
+
+import com.felipe.subscriptions.entities.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    List<Subscription> findUserIdAndActiveTrue(Long userId);
+
+    List<Subscription> findByActiveTrueAndNextBillingDateBetween(LocalDate start, LocalDate end);
+}
