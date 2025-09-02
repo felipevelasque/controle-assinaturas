d54a12546dbd3417dd2814f58f2213cdd9b1706a
Felipe Velasque <fe.velasquee@gmail.com>
2025-09-02 20:29:22 -0300

    feat: add PaymentMethod entity

---
+++ src/main/java/entities/PaymentMethod.java
@@ -0,0 +1,29 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.concurrent.Flow;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
+@EqualsAndHashCode(of = "id")
+public class PaymentMethod {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(length = 40)
+    private String brand; //Visa, Master, etc.
+
+    @Column(length = 4)
+    private String last4; // Últimos 4 dígitos;
+
+    private Integer expiryMonth; // 1..12
+    private Integer expiryYear; // yyyy
+
+}
