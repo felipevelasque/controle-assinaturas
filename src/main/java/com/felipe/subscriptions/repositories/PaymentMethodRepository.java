55f95add1f224ac88f4e02d6722e5cb7109d220e
Felipe Velasque <fe.velasquee@gmail.com>
2025-09-03 12:05:23 -0300

    refactor: change findUserById from Optional to List

--- src/main/java/com/felipe/subscriptions/repositories/PaymentMethodRepository.java
+++ src/main/java/com/felipe/subscriptions/repositories/PaymentMethodRepository.java
@@ -3,8 +3,9 @@ package com.felipe.subscriptions.repositories;
 import com.felipe.subscriptions.entities.PaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
-    Optional<PaymentMethod> findUserById(Long userId);
+    List<PaymentMethod> findUserById(Long userId);
 }
